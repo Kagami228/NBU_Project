2cc48a9eb548db0f31b89479f99dc1ce86b602f7
Kagami228 <43517039+Kagami228@users.noreply.github.com>
2020-03-23 22:09:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import trainingSet.py
-import neuron.py
-import net.py
+import treiningSet
+import neuron
+import net
 
 def showVectorVals(label , v):
     print(label+" ")
@@ -9,33 +9,27 @@ def showVectorVals(label , v):
         str=str+v[i]+" "
     print(str)
 
-trainingSet trainingData("testData.txt")# Обучаюзий набор из txt
-topology=[]#вектор 
-trainingData.getTopology(topology)#заполнить вектор topology
-net net(topology)#сеть вектора
+trainingData=treiningSet("testData.txt")
+topology=[]
+trainingData.getTopology(topology)
+net(topology)
 inputVals=[]
 targetVals=[]
-resultVals=[]#входное значение, целевое значение, Значение результата
-
-trainingPass = 0#Тренировочный проход
-
-	#while (!trainingDat.isEOF())#пока не конец файла
-while (!trainingData.isEOF()):#пока не конец файла
+resultVals=[]
+trainingPass = 0
+while trainingData.isEOF():
     trainingPass=trainingPass+1
-    print("Pass: "+trainingPass)#вывод пути
-    if (trainingData.getNextInputs(inputVals) != topology[0]): #если (полученные следующие входы) != значению(topology[0])) То  ↓
-        exit(0) #остановка
-    showVectorVals("Input:", inputVals)#Вызов функции Показать Входные значения!
-    net.feedForward(inputVals)#Подаём вперёд данные
-    trainingData.getTargetOutputs(targetVals)#получить целевые результаты
-    showVectorVals("Targets:", targetVals)#ответ целевой
-    assert(targetVals.size() == topology.back())#Проверка на ошибку, что то типа того
-    net.getResults(resultVals)#получаем результат из сети
-    showVectorVals("Outputs", resultVals)#Выписываем его
-    net.backProp(targetVals)#вроде бы задня опора сети Или Высчет ошибки
-    print("Net average error:" , net.getRecentAverageError())#вывод Чистая средняя ошибка 
-    print(Done)#Типа закончили
-   #if defined(_MSC_VER) || defined(_WIN32)//Проврка по макросам
-system("PAUSE")#пауза
-#endif
-	#return(0)#ТОЧКА
+    print("Pass: "+trainingPass)
+    if (trainingData.getNextInputs(inputVals) != topology[0]): 
+        exit(0) 
+    showVectorVals("Input:", inputVals)
+    net.feedForward(inputVals)
+    trainingData.getTargetOutputs(targetVals)
+    showVectorVals("Targets:", targetVals)
+    assert(targetVals.size() == topology.back())
+    net.getResults(resultVals)
+    showVectorVals("Outputs", resultVals)
+    net.backProp(targetVals)
+    print("Net average error:" , net.getRecentAverageError())
+    print(Done)
+system("PAUSE")
